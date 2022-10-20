ecd4af8c9887a1a9847570b1e3f01aa4735eec3b
yanyeef <yanyeef@nycstudents.net>
2022-10-19 22:22:36 -0400

    changes

--- src/RightTriangle.java
+++ src/RightTriangle.java
@@ -10,9 +10,9 @@ public class RightTriangle {
     }
 
     public double hypotenuse() {
-        double sumOfSides = Math.pow(base, 2) + Math.pow(height, 2);
-        double hypotenuse = Math.sqrt(sumOfSides);
-        return hypotenuse;
+        double sumOfSides = Math.pow(base, 2) + Math.pow(height, 2);// a^2 + b^2
+        double hypotenuse = Math.sqrt(sumOfSides); //square root of a^2 + b^2
+        return hypotenuse; //c
     }
 
 }
